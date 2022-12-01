1a7b85acacc17adaf6e90dd961b153bb917e83ab
Tristan Harvey S. Bombase <thsbombase@neu.edu.ph>
2022-12-01 21:38:50 +0800

    Remove Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Narra One Solutions- Home </title>
+    <title>Narra One Solutions</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
